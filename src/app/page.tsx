45b169a58f8b34fbedb58304164f815aa824f831
Your Name <your.email@example.com>
2024-12-26 23:16:28 -0600

    plyr25

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export async function generateMetadata(): Promise<Metadata> {
           width: 1200,
           height: 630,
           alt: "Press Play",
-        },
+        }
       ],
     },
     other: {
