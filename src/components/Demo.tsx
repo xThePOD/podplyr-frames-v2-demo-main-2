1187788f833e820faf0fd1b750eb68561e398aec
Your Name <your.email@example.com>
2024-12-26 18:52:58 -0600

    plyr11

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -774,7 +774,7 @@ export default function Demo() {
       audio.removeEventListener('loadedmetadata', updateDuration);
       audio.removeEventListener('ended', handleEnded);
     };
-  }, [audioRef.current, currentPlayingNFT]);
+  }, [currentPlayingNFT]);
 
   // Update the handlePlayAudio function
   const handlePlayAudio = async (nft: NFT) => {
