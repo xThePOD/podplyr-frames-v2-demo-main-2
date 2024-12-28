ff3069d1524244f24c25991920d1591532ccab41
Your Name <your.email@example.com>
2024-12-27 18:33:11 -0600

    plyr34

--- src/components/providers/WagmiProvider.tsx
+++ src/components/providers/WagmiProvider.tsx
@@ -1,14 +1,14 @@
 import { createConfig, http, WagmiProvider } from "wagmi";
 import { base } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { frameConnector } from "~/lib/connector";
+import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
 
 export const config = createConfig({
   chains: [base],
-  connectors: [frameConnector()],
   transports: {
     [base.id]: http(),
   },
+  connectors: [farcasterFrame()],
 });
 
 const queryClient = new QueryClient();
